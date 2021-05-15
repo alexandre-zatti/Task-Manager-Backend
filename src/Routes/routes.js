dec730dd89f11c97bddbc4ea0d3f856eae60e243
Daniel Paulo <danielpaulo199@gmail.com>
2021-05-14 21:45:38 -0300

    Adicionado rota para prioridades

--- src/Routes/routes.js
+++ src/Routes/routes.js
@@ -2,6 +2,7 @@ const routes = require('express').Router();
 const sistema = require('./Api/sistema/index');
 const projeto = require('./Api/projeto/index');
 const usuario = require('./Api/usuario/index')
+const prioridade = require('./Api/prioridade/index')
 
 routes.get('/', (req, res) => {
   res.status(200).json({ message: 'Home!' });
@@ -10,5 +11,6 @@ routes.get('/', (req, res) => {
 routes.use('/sistema', sistema);
 routes.use('/projeto', projeto);
 routes.use('/usuario', usuario);
+routes.use('/prioridade', prioridade);
 
 module.exports = routes;
