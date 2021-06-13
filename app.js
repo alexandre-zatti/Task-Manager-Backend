47f19ba9317c8766414d4eebc79e703f4b6a2daa
alexandre zatti <zatti.alexandre61@gmail.com>
2021-06-13 16:41:16 -0300

    adicionado CORS

--- app.js
+++ app.js
@@ -1,12 +1,17 @@
 require("dotenv").config();
-
 const
     express = require('express');
     app = express();
+    cors = require('cors');
     port = process.env.PORT;
     routes = require('./src/Routes/routes')
 
 app.use(express.json());
+
+app.use(cors({
+    credentials:true,
+    origin:['http://localhost:3000']
+}))
 app.use('/',routes);
 
 app.listen(port, () => {
