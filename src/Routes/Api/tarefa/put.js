e8d8edec5f97b1f218b0b47230d875fd082f5d8b
alexandre zatti <zatti.alexandre61@gmail.com>
2021-06-20 20:57:03 -0300

    alterando coisas

--- src/Routes/Api/tarefa/put.js
+++ src/Routes/Api/tarefa/put.js
@@ -3,7 +3,8 @@ const prisma = new PrismaClient()
 
 module.exports = async (req, res) => {
 
-  const { 
+  const {
+    projeto, 
     situacao,
     dev, 
     tipo,
@@ -19,26 +20,82 @@ module.exports = async (req, res) => {
   
   try {
     if(id){
+
+      let id_projeto;
+      let id_dev;
+      let id_tipo;
+      let id_status;
+      let id_prioridade;
+      let id_complexidade;
+  
+      if(projeto){
+        await prisma.projeto.findFirst({
+          where: {
+            nome: projeto,
+          },
+        }).then((result)=>id_projeto = result.id);
+      }
+  
+      if(dev){
+        await prisma.usuario.findFirst({
+          where: {
+            nome: dev,
+          },
+        }).then((result)=>id_dev = result.id);
+      }
+      
+      if(tipo){
+        await prisma.tipo.findFirst({
+          where: {
+            nome: tipo,
+          },
+        }).then((result)=>id_tipo = result.id);
+      }
+  
+      if(status){
+        await prisma.status.findFirst({
+          where: {
+            nome: status,
+          },
+        }).then((result)=>id_status = result.id);
+      }
+  
+      if(prioridade){
+        await prisma.prioridade.findFirst({
+          where: {
+            nome: prioridade,
+          },
+        }).then((result)=>id_prioridade = result.id);
+      }
+  
+      if(complexidade){
+        await prisma.complexidade.findFirst({
+          where: {
+            nome: complexidade,
+          },
+        }).then((result)=>id_complexidade = result.id);
+      }
+  
       await prisma.tarefa.update({
         where: {
           id: parseInt(id),
         },
         data:{
-          id_situacao: situacao,
-          id_dev: dev,
-          id_tipo: tipo,
-          id_status: status,
-          id_prioridade: prioridade,
-          id_complexidade: complexidade,
-          tempo_gasto: tempo_gasto,
+          id_projeto: id_projeto,
+          id_situacao: situacao ? situacao : 2,
+          id_dev: id_dev,
+          id_tipo: id_tipo,
+          id_status: id_status,
+          id_prioridade: id_prioridade,
+          id_complexidade: id_complexidade,
+          tempo_gasto: parseFloat(tempo_gasto),
           titulo: titulo,
           descricao: descricao,
           updated_on: new Date().toISOString()
         }
       })
-      .then(() => res.status(200).json('tarefa atualizado com sucesso!'));
+      .then(() => res.status(200).json('tarefa atualizada com sucesso!'));
     }
-   
   } catch (e) {
     res.status(400).json('Ops! ocorreu um erro ao atualizar o tarefa');
     throw e
