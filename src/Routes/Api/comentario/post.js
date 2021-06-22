61651360604912dcdee0f0012661d278af5d7071
alexandre zatti <zatti.alexandre61@gmail.com>
2021-06-21 23:54:51 -0300

    faltou

--- src/Routes/Api/comentario/post.js
+++ src/Routes/Api/comentario/post.js
@@ -3,13 +3,12 @@ const prisma = new PrismaClient()
 
 module.exports = async (req, res) => {
 
-  const { criador, comentario_pai, descricao, tarefa } = req.body
+  const { criador, descricao, tarefa } = req.body
 
   try {
     await prisma.comentario.create({
       data: {
         id_criador: criador,
-        id_comentario_pai: comentario_pai,
         id_tarefa: tarefa,
         descricao: descricao,
         created_on: new Date().toISOString()
